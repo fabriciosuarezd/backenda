68a12e6a0de331fd0652d6dfefdee4ba10d5a60a
Alumno1 <alumno1@iMac-01.local>
2025-10-17 20:32:07 -0600

    Sesion del viernes 17

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('hola fabricio')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+
+const port = process.env.PORT || 5001
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use('/api/tareas', require('./routes/tareasRoutes'))
+
+app.listen(port, ()=> console.log(`Servidor iniciado en el puerto ${port}`))
