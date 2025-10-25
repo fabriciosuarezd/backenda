3021e88c910e8ee17ed745100254a38ef8c2eefa
Alumno1 <alumno1@iMac-01.local>
2025-10-24 20:41:36 -0600

    Sesion del viernes 24

---
+++ backend/models/tareasModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const tareaSchema = mongoose.Schema({
+    texto: {
+        type: String,
+        required: [true, "Por favor teclea un texto para la tarea"]
+    }
+},{
+    timestamps : true
+})
+
+module.exports = mongoose.model('Tarea', tareaSchema)
