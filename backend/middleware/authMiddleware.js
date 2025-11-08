a884ca523914ccea5099d8b2ff88a93900b00629
Alumno1 <alumno1@iMac-01.local>
2025-11-07 20:19:57 -0600

    Sesion del viernes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,32 +1,33 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/usersModel')
 
-const protect = async(req,res,next) => {
+const protect = async(req, res, next) => {
+
+     //definir la variable token
     let token
 
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
-            //obtengo el token
+            //obtengo el token del encabezado de autorizacion
             token = req.headers.authorization.split(' ')[1]
-
-            //verificar el token
+            //verifico el token con la firma del secreto
             const decoded = jwt.verify(token,process.env.JWT_SECRET)
-
-            //obtener los datos del usuario desde el token
-            req.user = await User.findById(decoded.id).select(-password)
+            //busco el usuario con el id del token
+            req.user = await User.findById(decoded.id).select('-password')
 
             next()
-        }
-        catch(error) {
-            res.status(401)
-            throw new Error('Acceso no autorizado')
+
+        } catch (error) {
+           console.log(error) 
+           res.status(401)
+           throw new Error('Acceso no autorizado')
         }
     }
 
-    if(!token) {
+    if (!token) {
         res.status(401)
-            throw new Error('Acceso no autorizado, no se proporciono un token')
-    }
+           throw new Error('Acceso no autorizado, no proporcionaste el token')
+    }  
 }
 
-module.exports = {protect}
+module.exports = { protect}
